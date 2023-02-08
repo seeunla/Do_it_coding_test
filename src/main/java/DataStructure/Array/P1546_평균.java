0511f093c974f43575208f2f871edd435fe99a78
seeunla <107592967+seeunla@users.noreply.github.com>
2023-02-08 22:28:35 +0900

    P1546_평균

---
+++ src/main/java/DataStructure/Array/P1546_평균.java
@@ -0,0 +1,18 @@
+package DataStructure.Array;
+
+import java.util.Scanner;
+
+public class P1546_평균 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int A[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        long sum = 0;
+        long max = 0;
+
+    }
+}
